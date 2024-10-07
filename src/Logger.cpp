8f253ddab28d604b5d3bf417bb5ce39f032f2be8
kovertno <konradkurzak@onet.pl>
2024-10-07 18:25:52 +0200

    new files

---
+++ src/Logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Konrad Kurzak on 07/10/2024.
+//
+
+#include "Logger.h"
---
+++ src/Logger.h
@@ -0,0 +1,16 @@
+//
+// Created by Konrad Kurzak on 07/10/2024.
+//
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+
+
+class Logger {
+
+};
+
+
+
+#endif //LOGGER_H
